86929b7cbb7b80a3cec2455550ba62e10baecf96
DESKTOP-3TNQ0PC\Piotr <p.mroz.zywiol@gmail.com>
2025-11-29 23:48:37 +0100

    Add example ArmorClass demo component to character sheet and integrate with dynamic form service

--- src/app/character-sheet/character-sheet.ts
+++ src/app/character-sheet/character-sheet.ts
@@ -2,12 +2,14 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { JsonPipe } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { DynamicFormService } from '../shared/services/dynamic-form.service';
+import { ArmorClass } from './components/armor-class/armor-class';
 
 @Component({
   selector: 'app-character-sheet',
-  imports: [ReactiveFormsModule, JsonPipe],
+  imports: [ReactiveFormsModule, JsonPipe, ArmorClass],
   template: `<form [formGroup]="characterSheetForm.getFormGroup()">
     <!-- children here will dynamically add their own fields -->
+    <app-armor-class></app-armor-class>
     <pre>{{ characterSheetForm.rawValue() | json }}</pre>
     <pre>{{ characterSheetForm.status() }}</pre>
   </form>`,

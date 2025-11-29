47ab548b15e1412a3e26135d682597adb56a6869
DESKTOP-3TNQ0PC\Piotr <p.mroz.zywiol@gmail.com>
2025-11-29 23:38:15 +0100

    Refactor character sheet component to use DynamicFormService and update form handling

--- src/app/character-sheet/character-sheet.ts
+++ src/app/character-sheet/character-sheet.ts
@@ -1,17 +1,20 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { JsonPipe } from '@angular/common';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { startWith } from 'rxjs';
+import { DynamicFormService } from '../shared/services/dynamic-form.service';
+
 @Component({
   selector: 'app-character-sheet',
   imports: [ReactiveFormsModule, JsonPipe],
-  template: `<form [formGroup]="characterSheetForm">
+  template: `<form [formGroup]="characterSheetForm.getFormGroup()">
     <!-- children here will dynamically add their own fields -->
-    <pre>{{ characterSheetForm.value | json }}</pre>
-    <pre>{{ characterSheetForm.status }}</pre>
+    <pre>{{ characterSheetForm.rawValue() | json }}</pre>
+    <pre>{{ characterSheetForm.status() }}</pre>
   </form>`,
   styleUrls: ['./character-sheet.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CharacterSheet {
-  characterSheetForm = new FormGroup({});
+  characterSheetForm = inject(DynamicFormService);
 }

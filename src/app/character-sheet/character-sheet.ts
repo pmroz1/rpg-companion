b00216d7093444ce0d0b4c41e3203764943660de
DESKTOP-3TNQ0PC\Piotr <p.mroz.zywiol@gmail.com>
2025-11-29 23:26:03 +0100

    Implement character sheet form with reactive forms and JSON display

--- src/app/character-sheet/character-sheet.ts
+++ src/app/character-sheet/character-sheet.ts
@@ -1,10 +1,17 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import { JsonPipe } from '@angular/common';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-character-sheet',
-  imports: [],
-  template: `<p>character-sheet works!</p>`,
-  styleUrl: './character-sheet.scss',
+  imports: [ReactiveFormsModule, JsonPipe],
+  template: `<form [formGroup]="characterSheetForm">
+    <!-- children here will dynamically add their own fields -->
+    <pre>{{ characterSheetForm.value | json }}</pre>
+    <pre>{{ characterSheetForm.status }}</pre>
+  </form>`,
+  styleUrls: ['./character-sheet.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CharacterSheet {}
+export class CharacterSheet {
+  characterSheetForm = new FormGroup({});
+}

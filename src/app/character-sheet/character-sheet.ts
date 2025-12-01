d28090d595bfdfaef4083fa0bfaeac09f9aed01c
DESKTOP-3TNQ0PC\Piotr <p.mroz.zywiol@gmail.com>
2025-12-01 19:17:42 +0100

    Add Info component to CharacterSheet and update template

--- src/app/character-sheet/character-sheet.ts
+++ src/app/character-sheet/character-sheet.ts
@@ -4,14 +4,16 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { DynamicFormService } from '@shared/services/dynamic-form.service';
 import { ArmorClass } from './components/armor-class/armor-class';
 import { Appearance } from './components/appearance/appearance';
+import { DndCard } from '@app/shared/components/dnd-card/dnd-card';
+import { Info } from './components/info/info';
 
 @Component({
   selector: 'app-character-sheet',
-  imports: [ReactiveFormsModule, JsonPipe, ArmorClass, Appearance],
+  imports: [ReactiveFormsModule, JsonPipe, ArmorClass, Appearance, Info],
   template: `<form [formGroup]="characterSheetForm.getFormGroup()">
-    <!-- children here will dynamically add their own fields -->
     <app-armor-class></app-armor-class>
     <app-appearance></app-appearance>
+    <sheet-info />
     <pre>{{ characterSheetForm.rawValue() | json }}</pre>
     <pre>{{ characterSheetForm.status() }}</pre>
   </form>`,

a98568f1a3a82b9f5945af97b7b8cc6946f8b49c
DESKTOP-3TNQ0PC\Piotr <p.mroz.zywiol@gmail.com>
2025-12-02 22:28:44 +0100

    Add SpellsCantrips component with table display and styles

--- src/app/character-sheet/components/tabs/views/spells-cantrips/spells-cantrips.ts
+++ src/app/character-sheet/components/tabs/views/spells-cantrips/spells-cantrips.ts
@@ -1,10 +1,38 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TableModule } from 'primeng/table';
+
+export interface SpellCantrip {
+  level: number;
+  name: string;
+  category: string;
+  castingTime: string;
+  range: string;
+  concentration: boolean;
+  ritual: boolean;
+  requiredMaterial: boolean;
+  description: string;
+}
 
 @Component({
   selector: 'tab-spells-cantrips',
-  imports: [],
-  template: `<p>spells-cantrips works!</p>`,
-  styleUrl: './spells-cantrips.css',
+  imports: [TableModule],
+  template: `<p-table
+    [value]="spellsCantrips"
+    class="w-full"
+    [tableStyle]="{ 'min-width': '50rem' }"
+  >
+    <ng-template #header>
+      <tr>
+        <th>Code</th>
+        <th>Name</th>
+        <th>Category</th>
+        <th>Quantity</th>
+      </tr>
+    </ng-template>
+  </p-table>`,
+  styleUrls: ['./spells-cantrips.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SpellsCantrips {}
+export class SpellsCantrips {
+  spellsCantrips = [];
+}

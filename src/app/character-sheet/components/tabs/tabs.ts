8d28e63c24fa5f8b6a91decd7fe302aadf7940ac
DESKTOP-3TNQ0PC\Piotr <p.mroz.zywiol@gmail.com>
2025-12-02 22:21:35 +0100

    Add tabs for actions, spells, inventory, features, traits, and notes in Character Sheet

--- src/app/character-sheet/components/tabs/tabs.ts
+++ src/app/character-sheet/components/tabs/tabs.ts
@@ -1,13 +1,30 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { UpperCasePipe } from '@angular/common';
 import { TabsModule } from 'primeng/tabs';
 import { DndCard } from '@app/shared/components/dnd-card/dnd-card';
+import { ActionsTab } from './views/actions/actions';
+import { SpellsCantrips } from './views/spells-cantrips/spells-cantrips';
+import { InventoryTab } from './views/inventory/inventory';
+import { FeaturesTab } from './views/features/features';
+import { TraitsTab } from './views/traits/traits';
+import { NotesTab } from './views/notes/notes';
+
 @Component({
   selector: 'sheet-tabs',
-  imports: [TabsModule, DndCard, UpperCasePipe],
+  imports: [
+    TabsModule,
+    DndCard,
+    UpperCasePipe,
+    ActionsTab,
+    SpellsCantrips,
+    InventoryTab,
+    FeaturesTab,
+    TraitsTab,
+    NotesTab,
+  ],
   template: `
     <app-dnd-card title="Character Sheet Tabs" [displayDivider]="false" [displayTitle]="false">
-      <p-tabs [value]="1" class="w-full" scrollable>
+      <p-tabs value="0" class="w-full" scrollable>
         <p-tablist>
           @for (item of tabs; track $index) {
             <p-tab [value]="$index" class="flex-1">
@@ -15,6 +32,24 @@ import { DndCard } from '@app/shared/components/dnd-card/dnd-card';
             </p-tab>
           }
         </p-tablist>
+        <p-tabpanel [value]="0">
+          <tab-actions></tab-actions>
+        </p-tabpanel>
+        <p-tabpanel [value]="1">
+          <tab-spells-cantrips></tab-spells-cantrips>
+        </p-tabpanel>
+        <p-tabpanel [value]="2">
+          <tab-inventory></tab-inventory>
+        </p-tabpanel>
+        <p-tabpanel [value]="3">
+          <tab-features></tab-features>
+        </p-tabpanel>
+        <p-tabpanel [value]="4">
+          <tab-traits></tab-traits>
+        </p-tabpanel>
+        <p-tabpanel [value]="5">
+          <tab-notes></tab-notes>
+        </p-tabpanel>
       </p-tabs>
     </app-dnd-card>
   `,
---
+++ src/app/character-sheet/components/tabs/views/spells-cantrips/spells-cantrips.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'tab-spells-cantrips',
+  imports: [],
+  template: `<p>spells-cantrips works!</p>`,
+  styleUrl: './spells-cantrips.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SpellsCantrips {}

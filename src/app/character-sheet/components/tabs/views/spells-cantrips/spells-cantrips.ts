b1b794b4904dd79d426c237b3129db2ff9bb9d00
DESKTOP-3TNQ0PC\Piotr <p.mroz.zywiol@gmail.com>
2025-12-03 16:08:05 +0100

    Enhance SpellsCantrips component with improved styling and structure, update spell attributes in dictionary

--- src/app/character-sheet/components/tabs/views/spells-cantrips/spells-cantrips.ts
+++ src/app/character-sheet/components/tabs/views/spells-cantrips/spells-cantrips.ts
@@ -9,56 +9,64 @@ import { TableModule } from 'primeng/table';
 @Component({
   selector: 'tab-spells-cantrips',
   imports: [TableModule, CheckboxModule, FormsModule, Button],
-  template: `<p-table
-    [value]="spellsCantrips"
-    class="w-full"
-    [tableStyle]="{ 'min-width': '50rem' }"
-  >
-    <ng-template #header let-columns>
-      <tr>
-        @for (col of headers; track $index) {
-          <th>{{ col }}</th>
-        }
-      </tr>
-    </ng-template>
-    <ng-template let-spellCantrip pTemplate="body">
-      <tr>
-        <td>{{ spellCantrip.level }}</td>
-        <td>{{ spellCantrip.name }}</td>
-        <td>{{ spellCantrip.category }}</td>
-        <td>{{ spellCantrip.castingTime }}</td>
-        <td>{{ spellCantrip.range }}</td>
-        <td>
-          <p-checkbox [binary]="true" [(ngModel)]="spellCantrip.concentration" [disabled]="true" />
-        </td>
-        <td>
-          <p-checkbox [binary]="true" [(ngModel)]="spellCantrip.ritual" [disabled]="true" />
-        </td>
-        <td>
-          <p-checkbox
-            [binary]="true"
-            [(ngModel)]="spellCantrip.requiredMaterial"
-            [disabled]="true"
-          />
-        </td>
-        <td>
-          <p-button label="Description"></p-button>
-        </td>
-      </tr>
-    </ng-template>
+  template: `<div class="sc-container">
+    <p-table
+      [value]="spellsCantrips"
+      class="w-full"
+      [tableStyle]="{ 'min-width': '50rem' }"
+      class="sc-table"
+    >
+      <ng-template #header let-columns>
+        <tr>
+          @for (col of headers; track $index) {
+            <th>{{ col }}</th>
+          }
+        </tr>
+      </ng-template>
+      <ng-template let-spellCantrip pTemplate="body">
+        <tr>
+          <td>{{ spellCantrip.level }}</td>
+          <td>{{ spellCantrip.name }}</td>
+          <td>{{ spellCantrip.category }}</td>
+          <td>{{ spellCantrip.castingTime }}</td>
+          <td>{{ spellCantrip.range }}</td>
+          <td>
+            <p-checkbox
+              [binary]="true"
+              [(ngModel)]="spellCantrip.concentration"
+              [disabled]="true"
+            />
+          </td>
+          <td>
+            <p-checkbox [binary]="true" [(ngModel)]="spellCantrip.ritual" [disabled]="true" />
+          </td>
+          <td>
+            <p-checkbox
+              [binary]="true"
+              [(ngModel)]="spellCantrip.requiredMaterial"
+              [disabled]="true"
+            />
+          </td>
+          <td>
+            <p-button label="Description"></p-button>
+          </td>
+        </tr>
+      </ng-template>
 
-    <ng-template pTemplate="footer">
-      <div class="flex justify-end p-2">
-        <button pButton type="button" label="Add Spell/Cantrip" class="p-button-sm"></button>
-      </div>
-    </ng-template>
-  </p-table>`,
+      <ng-template pTemplate="footer">
+        <div class="flex justify-end p-2">
+          <button pButton type="button" label="Add Spell/Cantrip" class="p-button-sm"></button>
+        </div>
+      </ng-template>
+    </p-table>
+  </div>`,
   styleUrls: ['./spells-cantrips.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SpellsCantrips {
   private readonly formService = inject(DynamicFormService);
   spellsCantrips: SpellCantrip[] = [...DND_SPELLS_CANTRIPS];
+  knownSpellsCantrips: SpellCantrip[] = [];
 
   headers = [
     'Level',

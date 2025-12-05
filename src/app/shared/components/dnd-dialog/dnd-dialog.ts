0cee62650a2bb5314bb409b10d04e65e40f2621e
DESKTOP-3TNQ0PC\Piotr <p.mroz.zywiol@gmail.com>
2025-12-05 11:36:31 +0100

    feat: simplify DndDialogComponent template and remove unused imports

--- src/app/shared/components/dnd-dialog/dnd-dialog.ts
+++ src/app/shared/components/dnd-dialog/dnd-dialog.ts
@@ -1,31 +1,34 @@
-import { AfterViewChecked, AfterViewInit, Component, inject, input, signal } from '@angular/core';
+import { AfterViewInit, Component, inject, input, signal } from '@angular/core';
 import { DynamicDialogRef, DynamicDialogConfig } from 'primeng/dynamicdialog';
-import { DndCard } from '../dnd-card/dnd-card';
+import { Button } from 'primeng/button';
 
 @Component({
   selector: '<app-dnd-dynamic-dialog>',
-  imports: [DndCard],
-  template: `
-    <app-dnd-card [title]="header()" [displayDivider]="true" [displayTitle]="false">
-      <div class="p-4 text-lg">{{ content() }}</div>
-      <div class="w-full flex justify-end p-4"></div>
-    </app-dnd-card>
+  template: ` <div>
+    <div class="p-4 text-lg">{{ content() }}</div>
+    <div class="px-4 flex justify-end">
+      <p-button label="Ok" (click)="close()"></p-button>
+    </div>
+  </div>`,
+  styles: `
+    :host {
+      display: block;
+    }
   `,
+  imports: [Button],
 })
 export class DndDialogComponent implements AfterViewInit {
-  ref: DynamicDialogRef | undefined;
+  ref = inject(DynamicDialogRef);
   config = inject(DynamicDialogConfig);
 
-  header = signal<string>('');
   content = signal<string>('');
 
   ngAfterViewInit(): void {
     console.log('DndDialogComponent config data:', this.config?.data);
-    this.header.set(this.config?.data?.displayHeader || 'Dialog');
     this.content.set(this.config?.data?.displayText || 'Dialog content goes here.');
   }
 
   close() {
-    this.ref?.close({ result: 'ok' });
+    this.ref.close();
   }
 }

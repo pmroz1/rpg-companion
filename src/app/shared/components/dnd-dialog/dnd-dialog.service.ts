a5f14e9ef2326dc924a1226827dd8c529f46ff0b
DESKTOP-3TNQ0PC\Piotr <p.mroz.zywiol@gmail.com>
2025-12-05 11:43:37 +0100

    feat: add DndDialogService for managing dynamic dialog interactions

---
+++ src/app/shared/components/dnd-dialog/dnd-dialog.service.ts
@@ -0,0 +1,19 @@
+import { inject, Injectable } from '@angular/core';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { DndDialogComponent } from './dnd-dialog';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DndDialogService {
+  primengDialog = inject(DialogService);
+
+  openSimple(header: string, content: string): DynamicDialogRef<any> | null {
+    const ref = this.primengDialog.open(DndDialogComponent, {
+      header: header,
+      width: '50vw',
+      data: { contentText: content },
+    });
+    return ref;
+  }
+}

5537f8205c16b431a497cc3916c03f10a859a448
DESKTOP-3TNQ0PC\Piotr <p.mroz.zywiol@gmail.com>
2025-12-21 08:16:08 +0100

    feat: implement CharacterSheetState interface and integrate it into state management

---
+++ src/app/character-sheet/models/character-sheet-state.interface.ts
@@ -0,0 +1,23 @@
+import { CoinsInfo } from '../components/coins/models/coins-info';
+import { CharacterInfo } from '../components/info/model/character-info';
+import { SpellSlotInfo } from '../components/spell-slots/model/spell-slot-info';
+import { BackstoryInfo } from '../components/backstory/models/backstory-info';
+import { HitpointsInfo } from '../components/hitpoints/model/hitpoints-info';
+import { LanguagesInfo } from '../components/languages/models/languages-info';
+import { ProficienciesInfo } from '../components/proficiencies/model/proficiencies-info';
+import { SpellcastingStats } from '../components/spellcasting-ability/model/spellcasting';
+import { SpellCantrip } from '@data/models';
+
+export interface CharacterSheetState {
+  appearance: string;
+  coins: CoinsInfo;
+  info: CharacterInfo;
+  spellSlots: SpellSlotInfo[];
+  backstory: BackstoryInfo;
+  hitpoints: HitpointsInfo;
+  languages: LanguagesInfo;
+  proficiencies: ProficienciesInfo;
+  spellCasting: SpellcastingStats;
+  spellsCantrips: SpellCantrip[];
+  notes: string;
+}

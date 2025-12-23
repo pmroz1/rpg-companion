e8753558c6dd52f75370b351a322efaef1fded89
DESKTOP-3TNQ0PC\Piotr <p.mroz.zywiol@gmail.com>
2025-12-23 09:13:26 +0100

    feat: enhance Menubar component with hover effects and active route styling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,28 @@
 import { Component, signal } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { MenuItem } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 import { Menubar } from 'primeng/menubar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterLink, RouterOutlet, ButtonModule, Menubar],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet, ButtonModule, Menubar],
   template: `<div class="min-h-screen">
     <header
       class="bg-[var(--color-bg-elevated)] border-b-2 border-[var(--color-gold-dark)] px-6 py-2"
     >
-      <p-menubar [model]="menuItems" styleClass="border-none! bg-transparent! p-0 w-full">
+      <p-menubar [model]="menuItems">
         <ng-template #start>
           <h1 class="text-2xl gold-text tracking-wide">D&D 5e Companion</h1>
+        </ng-template>
+        <ng-template #item let-item>
+          <a
+            [routerLink]="item.routerLink"
+            class="p-menubar-item-link"
+            routerLinkActive="active-route"
+          >
+            <span class="p-menubar-item-label">{{ item.label }}</span>
+          </a>
         </ng-template>
       </p-menubar>
     </header>
